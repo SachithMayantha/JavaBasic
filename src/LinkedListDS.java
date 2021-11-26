a9329bd0e00a268263039e653f5c065182ca0535
Mayantha <wsmfernando@std.appsc.sab.ac.lk>
2021-11-27 01:02:43 +0530

    Initial commit

---
+++ src/LinkedListDS.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class LinkedListDS {
+    public static void main(String[] args) {
+        //LinkedList type is String or any
+        LinkedList<String> link = new LinkedList<String>();
+
+        link.add("Kamal");
+        link.add("Nimal");
+        link.add("Sumal");
+
+        System.out.println(link.getFirst());
+        System.out.println(link.size());
+    }
+
+}
+
