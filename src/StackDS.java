70f73493ccb91ed5d1d122149eff152394a9d2a7
Mayantha <wsmfernando@std.appsc.sab.ac.lk>
2021-11-27 01:13:29 +0530

    updated

---
+++ src/StackDS.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class StackDS {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+        stack.push("Kamal");
+        stack.push("Nimal");
+        stack.push("Sumal");
+
+        stack.pop();
+
+        System.out.println(stack.peek());
+    }
+}
